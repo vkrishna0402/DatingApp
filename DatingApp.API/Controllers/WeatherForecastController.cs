7a394c4863141ad8a700955e5b231eb228bc2931
vkrishna0402 <vinayakrishna2@gmail.com>
2020-08-03 12:23:56 +0530

    Initial Commit

---
+++ DatingApp.API/Controllers/WeatherForecastController.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DatingApp.API.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+
+namespace DatingApp.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private readonly DataContext _context;
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(DataContext context, ILogger<WeatherForecastController> logger)
+        {
+            _context = context;
+            _logger = logger;
+
+        }
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+
+
+        // public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        // {
+        //     _logger = logger;
+        // }
+
+        [HttpGet]
+
+        public async Task<IActionResult> GetValues()
+        {
+            var values = await _context.Values.ToListAsync();
+
+            return Ok(values);
+        }
+        // public IEnumerable<WeatherForecast> Get()
+        // {
+        //     var rng = new Random();
+        //     return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+        //     {
+        //         Date = DateTime.Now.AddDays(index),
+        //         TemperatureC = rng.Next(-20, 55),
+        //         Summary = Summaries[rng.Next(Summaries.Length)]
+        //     })
+        //     .ToArray();
+        // }
+
+        [HttpGet("{id}")]
+
+        public async Task<IActionResult> GetValue(int id)
+        {
+            var value = await _context.Values.FirstOrDefaultAsync(x => x.Id == id);
+            return Ok(value);
+        }
+
+    }
+}
